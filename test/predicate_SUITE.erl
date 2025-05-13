4eceaa596a87313b0730175c4476943d4b45ea11
Maria Scott <maria-12648430@hnc-agency.org>
2025-05-13 18:25:53 +0200

    Prune tests

--- test/predicate_SUITE.erl
+++ test/predicate_SUITE.erl
@@ -15,210 +15,87 @@
 -module(predicate_SUITE).
 
 -export([all/0]).
--export([id/1,
-	 matches/1, not_matches/1,
-	 eq/1, ne/1,
-	 lt/1, lte/1,
-	 gt/1, gte/1,
-	 oneof/1,
-	 switch_1/1, switch_2/1,
-	 with_2/1, with_3/1,
-	 all_test/1, not_all/1,
-	 any/1, not_any/1,
-	 complement/1]).
+-export([
+	 matches/1,
+	 not_matches/1,
+	 eq/1,
+	 ne/1,
+	 lt/1,
+	 lte/1,
+	 gt/1,
+	 gte/1
+	]).
 
 all() ->
-    [id,
-     matches, not_matches,
-     eq, ne,
-     lt, lte,
-     gt, gte,
-     oneof,
-     switch_1, switch_2,
-     with_2, with_3,
-     all_test, not_all,
-     any, not_any,
-     complement].
+    [
+     matches,
+     not_matches,
+     eq,
+     ne,
+     lt,
+     lte,
+     gt,
+     gte
+    ].
 
-id(_) ->
-    F = hnc_pred:id(),
-    true = F(true),
-    false = F(false).
-
-matches(_) ->
-    F = hnc_pred:matches(1),
-    false = F(0),
-    false = F(0.0),
-    true = F(1),
-    false = F (1.0),
-    false = F(2),
-    false = F(2.0).
-
-not_matches(_) ->
-    F = hnc_pred:not_matches(1),
-    true = F(0),
-    true = F(0.0),
-    false = F(1),
-    true = F(1.0),
-    true = F(2),
-    true = F(2.0).
-
-eq(_) ->
-    F = hnc_pred:eq(1),
-    false = F(0),
-    false = F(0.0),
-    true = F(1),
-    true = F(1.0),
-    false = F(2),
-    false = F(2.0).
-
-ne(_) ->
-    F = hnc_pred:ne(1),
-    true = F(0),
-    true = F(0.0),
-    false = F(1),
-    false = F(1.0),
-    true = F(2),
-    true = F(2.0).
-
-lt(_) ->
-    F = hnc_pred:lt(1),
-    true = F(0),
-    true = F(0.0),
-    false = F(1),
-    false = F(1.0),
-    false = F(2),
-    false = F(2.0).
-
-lte(_) ->
-    F = hnc_pred:lte(1),
-    true = F(0),
-    true = F(0.0),
-    true = F(1),
-    true = F(1.0),
-    false = F(2),
-    false = F(2.0).
-
-gt(_) ->
-    F = hnc_pred:gt(1),
-    false = F(0),
-    false = F(0.0),
-    false = F(1),
-    false = F(1.0),
-    true = F(2),
-    true = F(2.0).
-
-gte(_) ->
-    F = hnc_pred:gte(1),
-    false = F(0),
-    false = F(0.0),
-    true = F(1),
-    true = F(1.0),
-    true = F(2),
-    true = F(2.0).
-
-oneof(_) ->
-    F = hnc_pred:oneof([1, 2]),
-    false = F(0),
-    false = F(0.0),
-    true = F(1),
-    false = F(1.0),
-    true = F(2),
-    false = F(2.0),
-    false = F(3),
-    false = F(3.0).
-
-switch_1(_) ->
-    F = hnc_pred:switch(#{1 => true, 2 => false}),
-    {'EXIT', {{badkey, 0}, _}} = catch F(0),
-    {'EXIT', {{badkey, +0.0}, _}} = catch F(0.0),
-    true = F(1),
-    {'EXIT', {{badkey, 1.0}, _}} = catch F(1.0),
-    false = F(2),
-    {'EXIT', {{badkey, 2.0}, _}} = catch F(2.0),
-    {'EXIT', {{badkey, 3}, _}} = catch F(3),
-    {'EXIT', {{badkey, 3.0}, _}} = catch F(3.0),
+do_cmp_check(F, E1, E2, E3, E4, E5, E6, E7, E8, E9) ->
+    ok = do_cmp_check1(F, 0, E1, E2, E3),
+    ok = do_cmp_check1(F, +0.0, E4, E5, E6),
+    ok = do_cmp_check1(F, -0.0, E7, E8, E9),
     ok.
 
-switch_2(_) ->
-    F = hnc_pred:switch(#{1 => true, 2 => false}, false),
-    false = F(0),
-    false = F(0.0),
-    true = F(1),
-    false = F(1.0),
-    false = F(2),
-    false = F(2.0),
-    false = F(3),
-    false = F(3.0).
+do_cmp_check1(F, A, E1, E2, E3) ->
+    Fun = hnc_pred:F(A),
+    E1 = Fun(0),
+    E2 = Fun(+0.0),
+    E3 = Fun(-0.0),
+    ok.
 
-with_2(_) ->
-    B = fun(A, B) -> A > B end,
-    F = hnc_pred:with(B, [1]),
-    true = F(0),
-    true = F(0.0),
-    false = F(1),
-    false = F(1.0),
-    false = F(2),
-    false = F(2.0).
+matches(_) ->
+    ok = do_cmp_check(matches, true, false, false,
+		               false, true, false,
+			       false, false, true),
+    ok.
 
-with_3(_) ->
-    B = fun(A, B, C) -> A > B andalso B > C end,
-    F = hnc_pred:with(B, [3], [1]),
-    false = F(0),
-    false = F(0.0),
-    false = F(1),
-    false = F(1.0),
-    true = F(2),
-    true = F(2.0),
-    false = F(3),
-    false = F(3.0).
+not_matches(_) ->
+    ok = do_cmp_check(not_matches, false, true, true,
+				   true, false, true,
+				   true, true, false),
+    ok.
 
-all_test(_) ->
-    F = hnc_pred:all([fun(X) -> 3 > X end, fun(X) -> X > 1 end]),
-    false = F(0),
-    false = F(0.0),
-    false = F(1),
-    false = F(1.0),
-    true = F(2),
-    true = F(2.0),
-    false = F(3),
-    false = F(3.0).
+eq(_) ->
+    ok = do_cmp_check(eq, true, true, true,
+			  true, true, true,
+			  true, true, true),
+    ok.
 
-not_all(_) ->
-    F = hnc_pred:not_all([fun(X) -> 3 > X end, fun(X) -> X > 1 end]),
-    true = F(0),
-    true = F(0.0),
-    true = F(1),
-    true = F(1.0),
-    false = F(2),
-    false = F(2.0),
-    true = F(3),
-    true = F(3.0).
+ne(_) ->
+    ok = do_cmp_check(ne, false, false, false,
+			  false, false, false,
+			  false, false, false),
+    ok.
 
-any(_) ->
-    F = hnc_pred:any([fun(X) -> X < 1 end, fun(X) -> 2 < X end]),
-    true = F(0),
-    true = F(0.0),
-    false = F(1),
-    false = F(1.0),
-    false = F(2),
-    false = F(2.0),
-    true = F(3),
-    true = F(3.0).
+lt(_) ->
+    ok = do_cmp_check(lt, false, false, false,
+			  false, false, false,
+			  false, false, false),
+    ok.
 
-not_any(_) ->
-    F = hnc_pred:not_any([fun(X) -> X < 1 end, fun(X) -> 2 < X end]),
-    false = F(0),
-    false = F(0.0),
-    true = F(1),
-    true = F(1.0),
-    true = F(2),
-    true = F(2.0),
-    false = F(3),
-    false = F(3.0).
+lte(_) ->
+    ok = do_cmp_check(lte, true, true, true,
+			   true, true, true,
+			   true, true, true),
+    ok.
 
-complement(_) ->
-    F = hnc_pred:complement(fun erlang:is_atom/1),
-    false = F(x),
-    true = F(1),
-    true = F("abc").
+gt(_) ->
+    ok = do_cmp_check(gt, false, false, false,
+			  false, false, false,
+			  false, false, false),
+    ok.
+
+gte(_) ->
+    ok = do_cmp_check(gte, true, true, true,
+			   true, true, true,
+			   true, true, true),
+    ok.
+
--- test/prop_predicate.erl
+++ test/prop_predicate.erl
@@ -17,120 +17,120 @@
 -include_lib("proper/include/proper.hrl").
 
 prop_id() ->
-	?FORALL(
-		B,
-		boolean(),
-		B =:= (hnc_pred:id())(B)
-	).
+    ?FORALL(
+	B,
+	boolean(),
+	B =:= (hnc_pred:id())(B)
+    ).
 
 prop_complement() ->
-	?FORALL(
-		{F, B},
-		{function1(boolean()), boolean()},
-		not F(B) =:= (hnc_pred:complement(F))(B)
-	).
+    ?FORALL(
+	{F, B},
+	{function1(boolean()), boolean()},
+	not F(B) =:= (hnc_pred:complement(F))(B)
+    ).
 
 prop_matches() ->
-	?FORALL(
-		{T1, T2},
-		oneof([{any(), any()},
-		       ?LET(
-			       T,
-			       any(),
-			       {T, T}
-			)]),
-			(T1 =:= T2) =:= (hnc_pred:matches(T1))(T2)
-		andalso (T1 =/= T2) =:= (hnc_pred:not_matches(T1))(T2)
-	).
+    ?FORALL(
+	{T1, T2},
+	oneof([{any(), any()},
+	       ?LET(
+		   T,
+		   any(),
+		   {T, T}
+		)]),
+		(T1 =:= T2) =:= (hnc_pred:matches(T1))(T2)
+	andalso (T1 =/= T2) =:= (hnc_pred:not_matches(T1))(T2)
+    ).
 
 prop_compare() ->
-	?FORALL(
-		{T1, T2},
-		oneof([{any(), any()},
-		       ?LET(
-			       T,
-			       any(),
-			       {T, T}
-		       )]),
-			(T1 == T2) =:= (hnc_pred:eq(T1))(T2)
-		andalso (T1 /= T2) =:= (hnc_pred:ne(T1))(T2)
-		andalso (T1 > T2) =:= (hnc_pred:lt(T1))(T2)
-		andalso (T1 >= T2) =:= (hnc_pred:lte(T1))(T2)
-		andalso (T1 < T2) =:= (hnc_pred:gt(T1))(T2)
-		andalso (T1 =< T2) =:= (hnc_pred:gte(T1))(T2)
-	).
+    ?FORALL(
+	{T1, T2},
+	oneof([{any(), any()},
+	       ?LET(
+		   T,
+		    any(),
+		    {T, T}
+		)]),
+		(T1 == T2) =:= (hnc_pred:eq(T1))(T2)
+	andalso (T1 /= T2) =:= (hnc_pred:ne(T1))(T2)
+	andalso (T1 > T2) =:= (hnc_pred:lt(T1))(T2)
+	andalso (T1 >= T2) =:= (hnc_pred:lte(T1))(T2)
+	andalso (T1 < T2) =:= (hnc_pred:gt(T1))(T2)
+	andalso (T1 =< T2) =:= (hnc_pred:gte(T1))(T2)
+    ).
 
 prop_with_2() ->
-	?FORALL(
-		{F, Args, T},
-		?LET(
-			N,
-			range(0, 19),
-			{function(N + 1, boolean()), vector(N, any()), any()}
-		),
-		apply(F, Args ++ [T]) =:= (hnc_pred:with(F, Args))(T)
-	).
+    ?FORALL(
+	{F, Args, T},
+	?LET(
+	    N,
+	    range(0, 19),
+	    {function(N + 1, boolean()), vector(N, any()), any()}
+	),
+	apply(F, Args ++ [T]) =:= (hnc_pred:with(F, Args))(T)
+    ).
 
 prop_with_3() ->
-	?FORALL(
-		{F, Args1, Args2, T},
-		?LET(
-			N1,
-			range(0, 19),
-			?LET(
-				N2,
-				range(0, 19 - N1),
-				{function(N1 + N2 + 1, boolean()), vector(N1, any()), vector(N2, any()), any()}
-			)
-		),
-		apply(F, Args1 ++ [T] ++ Args2) =:= (hnc_pred:with(F, Args1, Args2))(T)
-	).
+    ?FORALL(
+	{F, Args1, Args2, T},
+	?LET(
+	    N1,
+	    range(0, 19),
+	    ?LET(
+		N2,
+		range(0, 19 - N1),
+		{function(N1 + N2 + 1, boolean()), vector(N1, any()), vector(N2, any()), any()}
+	    )
+	),
+	apply(F, Args1 ++ [T] ++ Args2) =:= (hnc_pred:with(F, Args1, Args2))(T)
+    ).
 
 prop_oneof() ->
-	?FORALL(
-		{C, T},
-		?LET(
-			{L, R, T},
-			{list(), list(), any()},
-			{oneof([L ++ R, L ++ [T|R]]), T}
-		),
-		lists:member(T, C) =:= (hnc_pred:oneof(C))(T)
-	).
+    ?FORALL(
+ 	{C, T},
+	?LET(
+	    {L, R, T},
+	    {list(), list(), any()},
+	    {oneof([L ++ R, L ++ [T|R]]), T}
+	),
+	lists:member(T, C) =:= (hnc_pred:oneof(C))(T)
+    ).
 
 prop_switch_2() ->
-	?FORALL(
-		{S, T},
-		?LET(
-			S,
-			?SUCHTHAT(M, map(any(), boolean()), M =/= #{}),
-			{S, oneof(maps:keys(S))}
-		),
-		maps:get(T, S) =:= (hnc_pred:switch(S))(T)
-	).
+    ?FORALL(
+	{S, T},
+	?LET(
+	    S,
+	    ?SUCHTHAT(M, map(any(), boolean()), M =/= #{}),
+ 	    {S, oneof(maps:keys(S))}
+	),
+	maps:get(T, S) =:= (hnc_pred:switch(S))(T)
+    ).
 
 prop_switch_3() ->
-	?FORALL(
-		{S, D, T},
-		?LET(
-			{S, D},
-			{map(any(), boolean()), boolean()},
-			{S, D, oneof([any() | maps:keys(S)])}
-		),
-		case S of
-			#{T := V} -> V =:= (hnc_pred:switch(S, D))(T);
-			#{} -> D =:= (hnc_pred:switch(S, D))(T)
-		end
-	).
+    ?FORALL(
+	{S, D, T},
+	?LET(
+	    {S, D},
+	    {map(any(), boolean()), boolean()},
+	    {S, D, oneof([any() | maps:keys(S)])}
+	),
+	case S of
+	    #{T := V} -> V =:= (hnc_pred:switch(S, D))(T);
+	    #{} -> D =:= (hnc_pred:switch(S, D))(T)
+	end
+    ).
 
 prop_all_any() ->
-	?FORALL(
-		{Fs, T},
-		{list(function1(boolean())), any()},
-		begin
-			Results = lists:map(fun(F) -> F(T) end, Fs),
-				(not lists:member(false, Results)) =:= (hnc_pred:all(Fs))(T)
-			andalso lists:member(false, Results) =:= (hnc_pred:not_all(Fs))(T)
-			andalso lists:member(true, Results) =:= (hnc_pred:any(Fs))(T)
-			andalso (not lists:member(true, Results)) =:= (hnc_pred:not_any(Fs))(T)
-		end
-	).
+    ?FORALL(
+	{Fs, T},
+	{list(function1(boolean())), any()},
+	begin
+		Results = lists:map(fun(F) -> F(T) end, Fs),
+			(not lists:member(false, Results)) =:= (hnc_pred:all(Fs))(T)
+		andalso lists:member(false, Results) =:= (hnc_pred:not_all(Fs))(T)
+		andalso lists:member(true, Results) =:= (hnc_pred:any(Fs))(T)
+		andalso (not lists:member(true, Results)) =:= (hnc_pred:not_any(Fs))(T)
+	end
+    ).
